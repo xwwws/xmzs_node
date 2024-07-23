bfff78dffb4e84241a1b8da86b8b5c0e968ca570
wz <17600820823@163.com>
2024-07-23 09:45:01 +0800

    nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import User from "./router/user.js";
 import LoggerMiddleware from "./middleware/logger.js";
 import preventHotLinking from "./middleware/HotLingking.js";
 import ReqResHeaders from "./router/reqResHeaders.js";
-
+const PORT = 3000
 const app = express();
 // 静态资源
 app.use("/static", express.static('static'))
@@ -12,6 +12,7 @@ app.use("/static", express.static('static'))
 app.use(preventHotLinking)
 // logger
 app.use(LoggerMiddleware)
+// json
 app.use(express.json());
 
 app.use('/list', List)
@@ -19,9 +20,11 @@ app.use('/user', User)
 app.use('/req-res-headers', ReqResHeaders)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('demo');
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log('');
+  console.log('');
+  console.log(`connect success on http://localhost:${PORT}`);
 });
